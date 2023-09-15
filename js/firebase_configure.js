8d8e4b87e6cb56a8f4360b85dacf5086b557fe25
tasheela <tasheelajay1999@gmail.com>
2023-09-15 21:42:04 +0530

    add firebase and initalized

---
+++ js/firebase_configure.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBzvCidfGmhlNemselszloz6C--W1N6Qlc",
+    authDomain: "hospital-management-syst-6bfc0.firebaseapp.com",
+    projectId: "hospital-management-syst-6bfc0",
+    storageBucket: "hospital-management-syst-6bfc0.appspot.com",
+    messagingSenderId: "457465003070",
+    appId: "1:457465003070:web:172e61c9645f56f985656f"
+};
+
+
+firebase.initializeApp(firebaseConfig);
